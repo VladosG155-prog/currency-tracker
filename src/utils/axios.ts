c65d705518462bb023966d3d654242ae0b90feee
Владислав Колос <vladislavkolos@Vladislavs-MacBook-Air.local>
2024-02-07 22:27:36 +0300

    fix env names

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.CURRENCY_API_URL,
+  baseURL: process.env.REACT_APP_CURRENCY_API_URL,
   timeout: 3000,
-  headers: { apikey: process.env.CURRENCY_API_KEY },
+  headers: { apikey: process.env.REACT_APP_CURRENCY_API_KEY },
 });
 
 export default instance;
-
