157c3db98e8f8bd8a89068af3ec8831de2641faa
Владислав Колос <vladislavkolos@Vladislavs-MacBook-Air.local>
2024-02-13 17:33:26 +0300

    change routes file with constants

--- src/utils/routesConfig.tsx
+++ src/utils/routesConfig.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@root/App';
 import FallBack from '@root/components/FallBack';
+import { routes } from '@root/constants/routes';
 import BankCardPage from '@root/pages/BankCardPage';
 import HomePage from '@root/pages/HomePage';
 import TimeLinePage from '@root/pages/TimeLinePage';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: routes.home,
     element: <App />,
     errorElement: <FallBack />,
     children: [
@@ -15,8 +16,8 @@ export const router = createBrowserRouter([
         element: <HomePage />,
         index: true,
       },
-      { element: <TimeLinePage />, path: '/timeline' },
-      { element: <BankCardPage />, path: '/bankcard' },
+      { element: <TimeLinePage />, path: routes.timeline },
+      { element: <BankCardPage />, path: routes.bankcard },
     ],
   },
 ]);
